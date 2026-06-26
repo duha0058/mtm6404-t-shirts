0fa42e79f1c6433813d7b2be15cf4a7877ef24eb
duha0058 <duha0058@algonquinlive.com>
2026-06-25 20:26:28 -0400

    complete your assignment

--- t-shirts.js
+++ t-shirts.js
@@ -29,7 +29,7 @@ const tshirts = [
   },
   {
     title: 'Grey T-Shirt',
-    image: 'blue-t-shirt.jpg',
+    image: 'grey-t-shirt.jpg',
     price: 4.99,
     stock: 2,
     quantity: 1
@@ -63,3 +63,121 @@ const tshirts = [
     quantity: 1
   }
 ]
+
+function TShirt (props) {
+  const shirt = props.shirt
+  const onQuantityChange = props.onQuantityChange
+  const onBuy = props.onBuy
+
+  function quantityHandler (e) {
+    onQuantityChange(shirt, e.target.value)
+  }
+
+  function buyHandler () {
+    onBuy(shirt)
+  }
+
+  const options = []
+
+  for (let i = 1; i <= shirt.stock; i++) {
+    options.push(
+      <option value={i} key={i}>{i}</option>
+    )
+  }
+
+  return (
+    <div className="shirt">
+      <h2>{shirt.title}</h2>
+
+      <img src={'images/' + shirt.image} alt={shirt.title} />
+
+      <p>${shirt.price}</p>
+
+      {
+        shirt.stock === 0
+          ? <p className="stock">Out of Stock</p>
+          : (
+            <div>
+              <p className="stock">Stock: {shirt.stock}</p>
+
+              <select value={shirt.quantity} onChange={quantityHandler}>
+                {options}
+              </select>
+
+              <button onClick={buyHandler}>Buy</button>
+            </div>
+          )
+      }
+    </div>
+  )
+}
+
+function App () {
+  const [shirts, setShirts] = React.useState(() =>
+    JSON.parse(localStorage.getItem('shirts')) || tshirts
+  )
+
+  function quantityChangeHandler (shirt, quantity) {
+    setShirts((prevState) => {
+      return prevState.map((item) => {
+        if (item.title === shirt.title) {
+          return {
+            title: item.title,
+            image: item.image,
+            price: item.price,
+            stock: item.stock,
+            quantity: parseInt(quantity)
+          }
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  function buyHandler (shirt) {
+    setShirts((prevState) => {
+      return prevState.map((item) => {
+        if (item.title === shirt.title) {
+          return {
+            title: item.title,
+            image: item.image,
+            price: item.price,
+            stock: item.stock - item.quantity,
+            quantity: 1
+          }
+        } else {
+          return item
+        }
+      })
+    })
+  }
+
+  React.useEffect(() => {
+    localStorage.setItem('shirts', JSON.stringify(shirts))
+  }, [shirts])
+
+  const shirtList = shirts.map((shirt) => {
+    return (
+      <TShirt
+        key={shirt.title}
+        shirt={shirt}
+        onQuantityChange={quantityChangeHandler}
+        onBuy={buyHandler}
+      />
+    )
+  })
+
+  return (
+    <React.Fragment>
+      <h1>T-Shirts</h1>
+
+      <div className="store">
+        {shirtList}
+      </div>
+    </React.Fragment>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
